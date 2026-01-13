Nexters/ChakChak-Android

[#4] chore: `:build-logic` 모듈 셋팅 및 버전 카탈로그 의존성 최적화
- 작업 사유: 멀티모듈 구조에서 Gradle Configuration을 일원화하여 관리하기 위함

app/build.gradle.kts
@@ -1,59 +1,19 @@
+import com.chakchak.convention.extensions.basePackage
+import com.chakchak.convention.extensions.setNamespace
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.chakchak.android.application)
 }
 
 android {
-    namespace = "com.chakchak"
-    compileSdk = 35
+    setNamespace("")
 
     defaultConfig {
-        applicationId = "com.chakchak"
-        minSdk = 31
-        targetSdk = 35
+        applicationId = "$basePackage.app"
         versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
+        versionName = "1.0.0"
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }

build-logic/convention/src/main/java/com/chakchak/AndroidComposeConventionPlugin.kt
@@ -1,0 +1,13 @@
+package com.chakchak
+
+import com.chakchak.convention.configureCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            configureCompose()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chakchak/AndroidLibraryConventionPlugin.kt
@@ -1,0 +1,16 @@
+package com.chakchak
+
+import com.chakchak.convention.configureAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.library")
+
+            configureAndroid()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chakchak/AndroidLibraryFeatureConventionPlugin.kt
@@ -1,0 +1,20 @@
+package com.chakchak
+
+import com.chakchak.convention.configureAndroid
+import com.chakchak.convention.configureCompose
+import com.chakchak.convention.configureHiltAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+
+class AndroidLibraryFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.library")
+
+            configureAndroid()
+            configureCompose()
+            configureHiltAndroid()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chakchak/AndroidRoomConventionPlugin.kt
@@ -1,0 +1,22 @@
+package com.chakchak
+
+import com.chakchak.convention.extensions.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidRoomConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.google.devtools.ksp")
+
+            dependencies {
+                val libs = project.extensions.libs
+                "implementation"(libs.findLibrary("room.runtime").get())
+                "implementation"(libs.findLibrary("room.ktx").get())
+                "ksp"(libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+}

build-logic/convention/src/main/java/com/chakchak/convention/Compose.kt
@@ -1,0 +1,36 @@
+package com.chakchak.convention
+
+import com.chakchak.convention.extensions.androidExtension
+import com.chakchak.convention.extensions.libs
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureCompose() {
+    with(pluginManager) {
+        apply("org.jetbrains.kotlin.plugin.compose")
+    }
+
+    androidExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.1"
+        }
+
+        dependencies {
+            val libs = project.extensions.libs
+            "implementation"(libs.findLibrary("androidx.activity.compose").get())
+
+            val bom = libs.findLibrary("compose-bom").get()
+            "implementation"(platform(bom))
+
+            "implementation"(libs.findLibrary("compose.material3").get())
+            "implementation"(libs.findLibrary("compose.material.icons").get())
+            "implementation"(libs.findLibrary("compose.ui").get())
+            "implementation"(libs.findLibrary("compose.ui.tooling.preview").get())
+            "debugImplementation"(libs.findLibrary("compose.ui.tooling").get())
+        }
+    }
+}

build-logic/convention/src/main/java/com/chakchak/convention/extensions/AppNameExtension.kt
@@ -1,0 +1,21 @@
+package com.chakchak.convention.extensions
+
+import org.gradle.api.Project
+
+/** 기본 패키지 */
+val basePackage: String get() = "com.chakchak"
+
+/**
+ * 모듈의 namespace를 셋팅
+ * @param name [basePackage]를 제외한 나머지 패키지
+ */
+fun Project.setNamespace(name: String = "") {
+    androidExtension.apply {
+        namespace = StringBuilder().apply {
+            append(basePackage)
+            if (name.isNotBlank()) {
+                append(".$name")
+            }
+        }.toString()
+    }
+}

build-logic/convention/src/main/java/com/chakchak/convention/extensions/Extension.kt
@@ -1,0 +1,29 @@
+package com.chakchak.convention.extensions
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.ExtensionContainer
+import org.gradle.kotlin.dsl.getByType
+
+/** com.android.application 플러그인을 적용한 모듈의 gradle 속성에 접근하기 위한 Extensions (ex. :app 모듈) */
+internal val Project.applicationExtension: CommonExtension<*, *, *, *, *, *>
+    get() = extensions.getByType<ApplicationExtension>()
+
+/** com.android.library 플러그인을 적용한 모듈의 gradle 속성에 접근하기 위한 Extensions (ex. :feature 하위 모듈) */
+internal val Project.libraryExtension: CommonExtension<*, *, *, *, *, *>
+    get() = extensions.getByType<LibraryExtension>()
+
+/** 라이브러리 모듈과 애플리케이션 모듈의 gradle 속성에 접근하기 위한 Extensions */
+internal val Project.androidExtension: CommonExtension<*, *, *, *, *, *>
+    get() = runCatching { libraryExtension }
+        .recoverCatching { applicationExtension }
+        .onFailure { println("Could not find Library or Application extension from this project") }
+        .getOrThrow()
+
+/** 버전 카탈로그를 참조 하기 위한 Extension */
+internal val ExtensionContainer.libs: VersionCatalog
+    get() = getByType<VersionCatalogsExtension>().named("libs")

Nexters/Chac-Android

app/build.gradle.kts
@@ -1,59 +1,20 @@
+import com.chac.convention.extensions.basePackage
+import com.chac.convention.extensions.setNamespace
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.chac.android.application)
+    alias(libs.plugins.chac.android.compose)
 }
 
 android {
-    namespace = "com.chac"
-    compileSdk = 36
+    setNamespace("")
 
     defaultConfig {
-        applicationId = "com.chac"
-        minSdk = 31
-        targetSdk = 36
+        applicationId = "$basePackage.app"
         versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
+        versionName = "1.0.0"
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }

build-logic/convention/src/main/java/com/chac/AndroidComposeConventionPlugin.kt
@@ -1,0 +1,13 @@
+package com.chac
+
+import com.chac.convention.configureCompose
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidComposeConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            configureCompose()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chac/AndroidLibraryConventionPlugin.kt
@@ -1,0 +1,16 @@
+package com.chac
+
+import com.chac.convention.configureAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.library")
+
+            configureAndroid()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chac/AndroidLibraryFeatureConventionPlugin.kt
@@ -1,0 +1,20 @@
+package com.chac
+
+import com.chac.convention.configureAndroid
+import com.chac.convention.configureCompose
+import com.chac.convention.configureHiltAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+
+class AndroidLibraryFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.android.library")
+
+            configureAndroid()
+            configureCompose()
+            configureHiltAndroid()
+        }
+    }
+}

build-logic/convention/src/main/java/com/chac/AndroidRoomConventionPlugin.kt
@@ -1,0 +1,22 @@
+package com.chac
+
+import com.chac.convention.extensions.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidRoomConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "com.google.devtools.ksp")
+
+            dependencies {
+                val libs = project.extensions.libs
+                "implementation"(libs.findLibrary("room.runtime").get())
+                "implementation"(libs.findLibrary("room.ktx").get())
+                "ksp"(libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+}

build-logic/convention/src/main/java/com/chac/convention/Compose.kt
@@ -1,0 +1,36 @@
+package com.chac.convention
+
+import com.chac.convention.extensions.androidExtension
+import com.chac.convention.extensions.libs
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureCompose() {
+    with(pluginManager) {
+        apply("org.jetbrains.kotlin.plugin.compose")
+    }
+
+    androidExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.1"
+        }
+
+        dependencies {
+            val libs = project.extensions.libs
+            "implementation"(libs.findLibrary("androidx.activity.compose").get())
+
+            val bom = libs.findLibrary("compose-bom").get()
+            "implementation"(platform(bom))
+
+            "implementation"(libs.findLibrary("compose.material3").get())
+            "implementation"(libs.findLibrary("compose.material.icons").get())
+            "implementation"(libs.findLibrary("compose.ui").get())
+            "implementation"(libs.findLibrary("compose.ui.tooling.preview").get())
+            "debugImplementation"(libs.findLibrary("compose.ui.tooling").get())
+        }
+    }
+}

build-logic/convention/src/main/java/com/chac/convention/extensions/AppNameExtension.kt
@@ -1,0 +1,21 @@
+package com.chac.convention.extensions
+
+import org.gradle.api.Project
+
+/** 기본 패키지 */
+val basePackage: String get() = "com.chac"
+
+/**
+ * 모듈의 namespace를 셋팅
+ * @param name [basePackage]를 제외한 나머지 패키지
+ */
+fun Project.setNamespace(name: String = "") {
+    androidExtension.apply {
+        namespace = StringBuilder().apply {
+            append(basePackage)
+            if (name.isNotBlank()) {
+                append(".$name")
+            }
+        }.toString()
+    }
+}

build-logic/convention/src/main/java/com/chac/convention/extensions/Extension.kt
@@ -1,0 +1,29 @@
+package com.chac.convention.extensions
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.plugins.ExtensionContainer
+import org.gradle.kotlin.dsl.getByType
+
+/** com.android.application 플러그인을 적용한 모듈의 gradle 속성에 접근하기 위한 Extensions (ex. :app 모듈) */
+internal val Project.applicationExtension: CommonExtension<*, *, *, *, *, *>
+    get() = extensions.getByType<ApplicationExtension>()
+
+/** com.android.library 플러그인을 적용한 모듈의 gradle 속성에 접근하기 위한 Extensions (ex. :feature 하위 모듈) */
+internal val Project.libraryExtension: CommonExtension<*, *, *, *, *, *>
+    get() = extensions.getByType<LibraryExtension>()
+
+/** 라이브러리 모듈과 애플리케이션 모듈의 gradle 속성에 접근하기 위한 Extensions */
+internal val Project.androidExtension: CommonExtension<*, *, *, *, *, *>
+    get() = runCatching { libraryExtension }
+        .recoverCatching { applicationExtension }
+        .onFailure { println("Could not find Library or Application extension from this project") }
+        .getOrThrow()
+
+/** 버전 카탈로그를 참조 하기 위한 Extension */
+internal val ExtensionContainer.libs: VersionCatalog
+    get() = getByType<VersionCatalogsExtension>().named("libs")