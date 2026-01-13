Nexters/ChakChak-Android

[#4] chore: `:feature:album` 모듈 생성

feature/album/build.gradle.kts
@@ -1,0 +1,9 @@
+import com.chakchak.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chakchak.android.library.feature)
+}
+
+android {
+    setNamespace("feature.album")
+}

feature/album/src/main/java/com/chakchak/feature/album/MyClass.kt
@@ -1,0 +1,3 @@
+package com.chakchak.feature.album
+
+class MyClass

Nexters/Chac-Android

feature/album/build.gradle.kts
@@ -1,0 +1,9 @@
+import com.chac.convention.extensions.setNamespace
+
+plugins {
+    alias(libs.plugins.chac.android.library.feature)
+}
+
+android {
+    setNamespace("feature.album")
+}

feature/album/src/main/java/com/chac/feature/album/MyClass.kt
@@ -1,0 +1,3 @@
+package com.chac.feature.album
+
+class MyClass